eb33e62882f653937502b0e2b0f7cf84cfaf3794
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-19 15:14:07 -0400

    added autoload function

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-$obj = new main();
-//making first commit
+  //set Debugging messages to 'ON'
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL);
+  
+  //Define a class with autoload function
+  class Auto 
+  {
+  //enabling classes to automatically load if undefines
+	public static function autoload($class)
+	{
+	 include $class . '.php';
+	}
+  }
+  spl_autoload_register(array('Auto', 'autoload'));
 
+  
 ?>
