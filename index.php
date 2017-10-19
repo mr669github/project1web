1394f6e2984ea87a75fcb7706163f241998123ea
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-19 15:30:40 -0400

    added a construct

--- index.php
+++ index.php
@@ -13,6 +13,26 @@
 	}
   }
   spl_autoload_register(array('Auto', 'autoload'));
-
   
+  //Instantiate an object for the class main
+  $obj = new main();
+
+  class main
+  {
+  	public function __construct()
+	{
+		$reqPage= 'formUpload';
+		if(isset($_REQUEST['page']))
+		{
+		$reqPage = $_REQUEST['page'];
+		}
+		$page = new $reqPage;
+		//Find request method
+		if($_SERVER['REQUEST_METHOD'] == 'GET')
+		$page->get();
+		else
+		$page->post();
+	}
+  } 
+         
 ?>
