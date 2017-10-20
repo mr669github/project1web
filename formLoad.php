10b30a9fffaa38be1e7ae937653d426ea9727ecd
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-20 11:13:05 -0400

    add req comment

--- formLoad.php
+++ formLoad.php
@@ -20,12 +20,12 @@
          $target_file = $target_dir . $_FILES["uploadCSVfile"]["name"];
          $fileid = $_FILES["uploadCSVfile"]["name"];
          $fileType= pathinfo($target_file,PATHINFO_EXTENSION);
-          
+        //First checking for csv file then redirect user to another page if saved in target directory
         if(($fileType == "csv")&&(move_uploaded_file($_FILES["uploadCSVfile"]["tmp_name"],$target_file)))
         {
           stringFunctions::printthis("<hr><br/><h1>Congrats! CSV File <b>" . $filename . "  is uploaded! </h1><hr><br/>");
           header("Refresh: 2;url=index.php?page=htmlTable&filename=$fileid");
-       }
+        }
         else if ($fileType != "csv")
        {
             stringFunctions::printThis("Sorry, Error! And Upload only CSV Files!");
