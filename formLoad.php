88d7abd2220aeee9d889f049a4f6e0f06efb3dc7
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-20 11:11:20 -0400

    check for csv and redirect

--- formLoad.php
+++ formLoad.php
@@ -18,7 +18,16 @@
         {
          $target_dir = "uploads/";
          $target_file = $target_dir . $_FILES["uploadCSVfile"]["name"];
-         $filename = $_FILES["uploadCSVfile"]["name"];
+         $fileid = $_FILES["uploadCSVfile"]["name"];
          $fileType= pathinfo($target_file,PATHINFO_EXTENSION);
-        
+          
+        if(($fileType == "csv")&&(move_uploaded_file($_FILES["uploadCSVfile"]["tmp_name"],$target_file)))
+        {
+          stringFunctions::printthis("<hr><br/><h1>Congrats! CSV File <b>" . $filename . "  is uploaded! </h1><hr><br/>");
+          header("Refresh: 2;url=index.php?page=htmlTable&filename=$fileid");
+       }
+        else if ($fileType != "csv")
+       {
+            stringFunctions::printThis("Sorry, Error! And Upload only CSV Files!");
+       }
 ?>
