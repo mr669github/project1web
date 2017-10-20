8a398521130a8edd5be79dcfa5855ea106b48098
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-20 11:59:33 -0400

    added neccessary comments

--- htmlTable.php
+++ htmlTable.php
@@ -1,12 +1,14 @@
 <?php
+    //Display of class in a table form
     class htmlTable extends page 
     {
     	 public function get()
     	 {
      		$filename = $_GET['filename'];
      		chdir('uploads');
+     		//now read file
      		$rfile= fopen($filename,"r");
-
+     		//dispTable() is called to display table
      		stylTags::dispTable();
      		$row=1;
     	        while(($data=fgetcsv($rfile))!== FALSE)
@@ -15,6 +17,7 @@
      			 {
       			   if($row==1)
       				 {
+      				//using stylTags class for html tag displays 	
         			stylTags::dispheader($value);
       				 }
      			   else
