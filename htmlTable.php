7ec7e24143149af604e29db3d7f37a2797f6ec93
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-20 11:52:15 -0400

    call style html tags func to display

--- htmlTable.php
+++ htmlTable.php
@@ -6,4 +6,26 @@
      		$filename = $_GET['filename'];
      		chdir('uploads');
      		$rfile= fopen($filename,"r");
+
+     		stylTags::dispTable();
+     		$row=1;
+    	        while(($data=fgetcsv($rfile))!== FALSE)
+    		 {
+      			foreach($data as $value)
+     			 {
+      			   if($row==1)
+      				 {
+        			stylTags::dispheader($value);
+      				 }
+     			   else
+      				 {
+       				stylTags::dispContent($value);
+      				 }
+     		          }
+      		$row++;
+      		stylTags::endRow();
+   	         }
+     fclose($rfile);
+          }
+    }
 ?>
