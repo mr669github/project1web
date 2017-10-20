14247d688b241555794b6f7d9a614e04055162e2
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-20 11:19:16 -0400

    add req comment

--- stringFunctions.php
+++ stringFunctions.php
@@ -1,6 +1,7 @@
 <?php
      class stringFunctions
-     {
+     { 
+       //class to print the text
        static public function printThis($inputText) 
        {
         return print($inputText);
