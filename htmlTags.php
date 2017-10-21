2bf57379871f61e58f0acc7ca9c80a6f5c30ad48
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-21 00:45:30 -0400

    tag for table display

--- htmlTags.php
+++ htmlTags.php
@@ -4,16 +4,20 @@
   	 static public function pageHead($text)
   	   {
           return '<h1>' . $text . '</h1>';
-           }
-          static public function dispHeader($text)
-           {
+       }
+      static public function dispHeader($text)
+      {
    	     echo '<th  style="font-size: 20px; background-color: #f0f0f0 " >'.$text.'</th>';
-  	   }
+  	  }
   	   static public function dispContent($text)
-           {
-              echo '<td>'.$text.'</td>';
-              echo '<even td style=" backgorund-color: #F6F6F6">';
-              echo '<odd td style=" background-color:#FFF">';
-            }
-         
+         {
+          echo '<td>'.$text.'</td>';
+          echo '<even td style=" backgorund-color: #F6F6F6">';
+          echo '<odd td style=" background-color:#FFF">';
+         }
+          static public function dispTable()
+  	  {
+             echo '<tr style="background-color: #f0f0f0">';
+  	     echo "<table cellpadding='10px' border='2px #ccc solid' margin='5px'  style='border-collapse: collapse'>";
+  	  }
 ?>
