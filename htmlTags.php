dc1c663cbf937ebb57c61ac00248664724773448
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-21 00:42:35 -0400

    tag for content display

--- htmlTags.php
+++ htmlTags.php
@@ -4,5 +4,16 @@
   	 static public function pageHead($text)
   	   {
           return '<h1>' . $text . '</h1>';
-       }
+           }
+          static public function dispHeader($text)
+           {
+   	     echo '<th  style="font-size: 20px; background-color: #f0f0f0 " >'.$text.'</th>';
+  	   }
+  	   static public function dispContent($text)
+           {
+              echo '<td>'.$text.'</td>';
+              echo '<even td style=" backgorund-color: #F6F6F6">';
+              echo '<odd td style=" background-color:#FFF">';
+            }
+         
 ?>
