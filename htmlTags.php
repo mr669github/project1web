9bb4b8c794c9b78f57698ca78c645e9f1b11d47c
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-21 00:49:07 -0400

    done tagsss

--- htmlTags.php
+++ htmlTags.php
@@ -4,20 +4,23 @@
   	 static public function pageHead($text)
   	   {
           return '<h1>' . $text . '</h1>';
-       }
-      static public function dispHeader($text)
-      {
+           }
+           static public function dispHeader($text)
+          {
    	     echo '<th  style="font-size: 20px; background-color: #f0f0f0 " >'.$text.'</th>';
   	  }
-  	   static public function dispContent($text)
-         {
-          echo '<td>'.$text.'</td>';
-          echo '<even td style=" backgorund-color: #F6F6F6">';
-          echo '<odd td style=" background-color:#FFF">';
-         }
+  	  static public function dispContent($text)
+          {
+             echo '<td>'.$text.'</td>';
+             echo '<even td style=" backgorund-color: #F6F6F6">';
+             echo '<odd td style=" background-color:#FFF">';
+          }
           static public function dispTable()
   	  {
-             echo '<tr style="background-color: #f0f0f0">';
   	     echo "<table cellpadding='10px' border='2px #ccc solid' margin='5px'  style='border-collapse: collapse'>";
-  	  }
+     	  }
+     	   static public function endRow()
+          {
+            echo '</tr>';
+          }
 ?>
