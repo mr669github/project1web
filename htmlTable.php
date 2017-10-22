de8e030f68d9e78591585f35947bef7dcd9b7edb
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-21 20:42:40 -0400

    clean code

--- htmlTable.php
+++ htmlTable.php
@@ -9,24 +9,25 @@
      		//now read file
      		$rfile= fopen($filename,"r");
      		//dispTable() is called to display table
-     		stylTags::dispTable();
-     		$row=1;
+     		htmlTags::dispTable();
+		//using this variable to keep track of header positions
+     		$i=1; 
     	        while(($data=fgetcsv($rfile))!== FALSE)
     		 {
       			foreach($data as $value)
      			 {
-      			   if($row==1)
+      			   if($i==1)
       				 {
       				//using stylTags class for html tag displays 	
-        			stylTags::dispheader($value);
+        			htmlTags::dispheader($value);
       				 }
      			   else
       				 {
-       				stylTags::dispContent($value);
+       				htmlTags::dispContent($value);
       				 }
      		          }
-      		$row++;
-      		stylTags::endRow();
+      		$i++;
+      		htmlTags::endRow();
    	         }
      fclose($rfile);
           }
