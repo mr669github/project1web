53cc39334c9637d5e3bfb3433cf807b9c0ccc80f
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-22 17:54:19 -0400

    indent

--- formLoad.php
+++ formLoad.php
@@ -4,29 +4,28 @@
         //This function is to request input file from user and display form
         public function get()
         {
-        $form = '<form action="index.php?page=formUpload" method="POST"
-        enctype="multipart/form-data">';
-        $form .= '<input type="file" name="uploadCSVfile" id="uploadfile">';
-        $form .= '<input type="submit" name="displayCSVtoHTML" id="submit">';       
-        $form .= '</form>';
-        $this->html .= htmlTags::pageHead('Submit File to Upload');
-        $this->html .= $form;
+         $form = '<form action="index.php?page=formLoad" method="POST" enctype="multipart/form-data">';
+         $form .= '<input type="file" name="uploadCSVfile" id="uploadfile">';
+         $form .= '<input type="submit" name="displayCSVtoHTML" id="submit">';       
+         $form .= '</form>';
+         $this->html .= htmlTags::pageHead('Submit File to Upload');
+         $this->html .= $form;
         }
 
         //This function is to upload to target directory, and change header 
-         public function post()
+        public function post()
         {
-         $target_dir = "uploads/";
-         $target_file = $target_dir . $_FILES["uploadCSVfile"]["name"];
-         $fileid = $_FILES["uploadCSVfile"]["name"];
-         $fileType= pathinfo($target_file,PATHINFO_EXTENSION);
-        //First checking for csv file then redirect user to another page if saved in target directory
-         if(($fileType == "csv")&&(move_uploaded_file($_FILES["uploadCSVfile"]["tmp_name"],$target_file)))
-           {
+          $target_dir = "uploads/";
+          $target_file = $target_dir . $_FILES["uploadCSVfile"]["name"];
+          $fileid = $_FILES["uploadCSVfile"]["name"];
+          $fileType= pathinfo($target_file,PATHINFO_EXTENSION);
+          //First checking for csv file then redirect user to another page if saved in target directory
+          if(($fileType == "csv")&&(move_uploaded_file($_FILES["uploadCSVfile"]["tmp_name"],$target_file)))
+          {
             stringFunctions::printthis("<hr><br/><h1>Congrats! CSV File <b>" . $fileid . "  is uploaded! </h1><hr><br/>");
-            header("Refresh: 2;url=index.php?page=htmlTable&filename=$filid");
-           }
-        else if ($fileType != "csv")
+            header("Refresh: 2;url=index.php?page=htmlTable&filename=$fileid");
+          }
+          else if ($fileType != "csv")
           {
             stringFunctions::printThis("Sorry, Error! And Upload only CSV Files!");
           }
