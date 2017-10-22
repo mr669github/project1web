ef432309e959a4f8c40428b5855605808a4989df
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-21 20:37:54 -0400

    clean code

--- htmlTags.php
+++ htmlTags.php
@@ -1,26 +1,26 @@
 <?php
+  //class includes functions for display of headers,table,content
   class htmlTags
     {
   	 static public function pageHead($text)
-  	   {
+  	 {
           return '<h1>' . $text . '</h1>';
-           }
-           static public function dispHeader($text)
-          {
-   	     echo '<th  style="font-size: 20px; background-color: #f0f0f0 " >'.$text.'</th>';
-  	  }
-  	  static public function dispContent($text)
-          {
-             echo '<td>'.$text.'</td>';
-             echo '<even td style=" backgorund-color: #F6F6F6">';
-             echo '<odd td style=" background-color:#FFF">';
-          }
-          static public function dispTable()
-  	  {
-  	     echo "<table cellpadding='10px' border='2px #ccc solid' margin='5px'  style='border-collapse: collapse'>";
-     	  }
-     	   static public function endRow()
-          {
-            echo '</tr>';
-          }
+         }
+         static public function dispHeader($text)
+         {
+   	  echo '<th  style="font-size: 20px; background-color:#FF9999 " >'.$text.'</th>';
+  	 }
+  	 static public function dispContent($text)
+         {
+          echo '<td>'.$text.'</td>';
+         }
+         static public function dispTable()
+  	 {
+  	  echo "<table cellpadding='10px' border='2px #ccc solid' margin='5px'  style='border-collapse: collapse'>";
+     	 }
+     	 static public function endRow()
+         {
+          echo '</tr>';
+         }
+     }
 ?>
